2df518d2ca2c9b12257b208823bbbcfe1659f8d0
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-02 14:14:21 +0700

    Initial Commit

---
+++ src/jobsheet5/OperatorAritmatika2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JOBSHEET5;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class OperatorAritmatika2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        double z = 2.5;
+        
+        int a = 0;
+        int b = 0;
+        
+        //Tambahkan sintaks operasi matematika di baris ini
+        
+        System.out.println(a);
+    }
+}
